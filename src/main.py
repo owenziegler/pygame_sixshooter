1a3f9773bf405e5149f3c496162d9562bc2f000c
Owen Ziegler <odz2@txstate.edu>
2025-01-18 21:12:03 -0600

    Added Game class to main.py

--- src/main.py
+++ src/main.py
@@ -4,29 +4,50 @@ main.py
 Owen Ziegler
 15 Jan 2025
 
-Main driver program, which sets up the window and starts the game loop.
+Description:
+Game class, main driver of the program
 '''
 import pygame
-import entity
+from entity import Entity
+from scene import Scene
+from sceneManager import SceneManager
 #constants
-WIDTH = 640
-HEIGHT = 480
-#initializations
-pygame.init()
-#display screen
-screen = pygame.display.set_mode((WIDTH,HEIGHT))
-clock = pygame.time.Clock()
-#display window title
-pygame.display.set_caption("sixshooter")
-#icon = pygame.image.load('images/iconImage.png') fill in when you have one
-#pygame.display.set_icon(icon)
-#game loop
-running = True
-while running:
-    clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    pygame.display.update()
-pygame.quit()
-exit()
+SCREENWIDTH = 640
+SCREENHEIGHT = 480
+FPS = 60
+
+class Game:
+    def __init__(self):
+        #initialization
+        pygame.init()
+        self.window = pygame.display.set_mode((SCREENWIDTH,SCREENHEIGHT))
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption("sixshooter")
+        #icon = pygame.image.load('imagepath') #fill in later
+        #pygame.display.set_icon(icon)
+        self.sceneManager = SceneManager('scene1')
+        self.scene1 = Scene(self.window, self.sceneManager)
+        self.scene1.setBackgroundColor((255,0,255))
+        self.scene2 = Scene(self.window, self.sceneManager)
+        self.scene2.setBackgroundColor((0,255,255))
+        self.scenes = {'scene1':self.scene1, 'scene2':self.scene2}
+        
+    def run(self):
+        running = True
+        while running:
+            self.clock.tick(FPS)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.KEYDOWN:
+                    self.sceneManager.setCurrentScene('scene2')
+                if event.type == pygame.KEYUP:
+                    self.sceneManager.setCurrentScene('scene1')
+            self.scenes[self.sceneManager.getCurrentScene()].draw()
+            pygame.display.update()
+        pygame.quit()
+        exit()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
